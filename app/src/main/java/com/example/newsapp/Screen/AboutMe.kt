95ed3877e0a1637630ad387f53ffe5f9bbdf226f
NaraSIMHA-008 <qwertyuiop@Mac.local>
2024-08-16 17:42:43 +0530

    first commit

---
+++ app/src/main/java/com/example/newsapp/Screen/AboutMe.kt
@@ -0,0 +1,182 @@
+package com.example.newsapp.Screen
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.newsapp.R
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+@Composable
+//fun AboutMe(navHostController: NavHostController) {
+fun AboutMe() {
+    val context = LocalContext.current
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+        .background(color = MaterialTheme.colorScheme.primary),
+                contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.primary)
+                .padding(horizontal = 16.dp, vertical = 20.dp)
+        ) {
+            // Header
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                        horizontalArrangement = Arrangement.Center
+            ) {
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "About Developer",
+                    style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
+                    color = Color.White
+
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            // Divider
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(2.dp)
+                    .background(color = Color.Green)
+            )
+
+            // Content
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 40.dp)
+            ) {
+                // Profile pic
+                Card(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .clip(CircleShape)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.myimage),
+                        contentDescription = "Developer Profile Pic",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Name
+                Text(
+                    text = "Nagendla Narasimha Reddy",
+                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+                    color = Color.Green
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // Bio
+                Text(
+                    text = ">A passionate developer from India Pursuing Btech 3rd year in Indian Institute Of Information Technology Lucknow",
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                    color = Color.Gray
+                )
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                // Follow me
+                Text(
+                    text = "Follow me",
+                    style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
+                    color = Color.White
+                )
+
+                // Social media buttons
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 15.dp)
+
+
+
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        IconButton(
+                            onClick = { openUrl(context, "https://x.com/narasimha_08") },
+                            modifier = Modifier.size(35.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.twitter),
+                                contentDescription = "Twitter"
+                            )
+                        }
+                        IconButton(
+                            onClick = { openUrl(context, "https://github.com/NaraSIMHA-008") },
+                            modifier = Modifier.size(34.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.github),
+                                contentDescription = "Github"
+                            )
+                        }
+                        IconButton(
+                            onClick = { openUrl(context, "https://www.instagram.com/nara_.simha_.reddy") },
+                            modifier = Modifier.size(34.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.instagram),
+                                contentDescription = "Instagram"
+                            )
+                        }
+                        IconButton(
+                            onClick = { openUrl(context, "https://www.linkedin.com/in/narasimha-reddy-327849264/") },
+                            modifier = Modifier.size(35.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.linkedin),
+                                contentDescription = "Linkedin"
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
+    fun openUrl(context: android.content.Context, url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        context.startActivity(intent)
+    }
+
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun AboutMePreview() {
+        NewsAppTheme {
+            AboutMe()
+        }
+    }
